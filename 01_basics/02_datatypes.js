f2f8201a737276d50d821aa0f2558af868f006b7
vaibhav <dev.vaibhavx@gmail.com>
2025-06-07 12:38:29 +0000

    learn datatypes in js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+let name = ("vaibhav")// string data type 
+let age =18// number data type 
+ let isLoggedIn =false//bpplean data type 
+
+
+//  typeof command is used to know the type of data type 
+
+// console.table(typeof name,age,isLoggedIn)
+console.log(typeof null);
